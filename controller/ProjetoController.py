3583409dcffb16212ac8f788107bd3f38ba867d3
Adjailson F. Melo <afm4@ecomp.poli.br>
2024-06-24 22:57:21 -0300

    Model e controller

---
+++ controller/ProjetoController.py
@@ -0,0 +1,15 @@
+
+#class para o requests com Cloud Firestore
+class ProjetoController:
+
+    def __init__(self):
+        pass
+
+    def newProject(self, model):
+        pass
+        
+    def listProject(self):
+        pass
+
+    def getProjectDoc(self, doc):
+        pass
